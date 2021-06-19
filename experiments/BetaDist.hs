ae6d0198a2aba2f602323d44439eb701d350b4c5
Noam Zeilberger <noam.zeilberger@gmail.com>
2021-06-19 14:51:25 +0200

    some example experiments

---
+++ experiments/BetaDist.hs
@@ -0,0 +1,8 @@
+-- experiment measuring "distance to normal form" = the number of beta-reductions
+-- needed to reach beta-normal form
+
+import LinLam
+import LinLam.Core
+import LinLam.Random
+
+main = mainExperiment (\t -> size t - size (normalize t))
---
+++ experiments/ClosedSubterms.hs
@@ -0,0 +1,11 @@
+-- experiment measuring the number of closed proper subterms of a
+-- random linear term (= bridges in rooted trivalent maps).
+
+-- For medium-sized terms and a large number of experiments, the result should
+-- approximate a Poisson distribution, see https://arxiv.org/abs/2106.08291
+
+import LinLam
+import LinLam.Core
+import LinLam.Random
+ 
+main = mainExperiment (\t -> length [u | u <- subterms' t, arity u == 0])
