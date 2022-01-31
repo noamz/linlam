53a330a2b10de0400930c2cd4b3776bc52b9494c
Noam Zeilberger <noam.zeilberger@gmail.com>
2022-01-31 20:54:05 +0100

    example generating random terms

---
+++ experiments/RandomTerm.hs
@@ -0,0 +1,22 @@
+-- generate and print some random linear terms
+
+import LinLam
+import LinLam.Core
+import LinLam.Random
+import LinLam.Pretty
+
+import Control.Monad
+import System.Environment
+import System.Exit
+
+main :: IO ()
+main = do
+  name <- getProgName
+  args <- getArgs
+  unless (length args >= 1) $ do
+    putStrLn ("Usage: " ++ name ++ " <size> [<trials>]")
+    exitFailure
+  let n = read (args !! 0)
+  let p = if length args >= 2 then read (args !! 1) else 1
+  ys <- experimentLT id (3*n+2) p
+  mapM_ printLT ys
